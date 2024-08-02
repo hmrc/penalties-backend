1108012991b67ff2cd8fb44f4404f43723e58d09
Dan Boresjö <6451217+dboresjo@users.noreply.github.com>
2024-08-02 17:47:49 +0100

    integration fixes

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -60,7 +60,7 @@ class AppConfig @Inject()(val config: Configuration, servicesConfig: ServicesCon
 
   lazy val desBearerToken: String = config.get[String]("des.outboundBearerToken")
 
-  lazy val stubBase: String = servicesConfig.baseUrl("penalties-stub")
+  lazy val stubBase: String = servicesConfig.baseUrl("income-tax-penalties-stubs")
 
   lazy val etmpBase: String = servicesConfig.baseUrl("etmp")
 
@@ -80,7 +80,7 @@ class AppConfig @Inject()(val config: Configuration, servicesConfig: ServicesCon
   lazy val SDESNotificationFileRecipient: String = config.get[String]("SDESNotification.file.recipient")
 
   private def getPenaltyDetailsUrl: String = {
-    if (!isEnabled(CallAPI1812ETMP)) stubBase + "/penalties-stub/penalty/details/"
+    if (!isEnabled(CallAPI1812ETMP)) stubBase + "/income-tax-penalties-stubs/penalty/details/"
     else etmpBase + "/penalty/details/"
   }
 
@@ -89,7 +89,7 @@ class AppConfig @Inject()(val config: Configuration, servicesConfig: ServicesCon
   //def getCtPenaltyDetailsUrl: String = getPenaltyDetailsUrl + "CT/UTR/"
 
   private def getFinancialDetailsUrl: String = {
-    if (!isEnabled(CallAPI1811ETMP)) stubBase + s"/penalties-stub/penalty/financial-data/"
+    if (!isEnabled(CallAPI1811ETMP)) stubBase + s"/income-tax-penalties-stubs/penalty/financial-data/"
     else etmpBase + s"/penalty/financial-data/"
   }
 
@@ -100,7 +100,7 @@ class AppConfig @Inject()(val config: Configuration, servicesConfig: ServicesCon
   //def getCtFinancialDetailsUrl(utr: String): String = getFinancialDetailsUrl + s"UTR/$utr/CT"
 
   def getAppealSubmissionURL(enrolmentKey: String, isLPP: Boolean, penaltyNumber: String): String = {
-    if (!isEnabled(CallPEGA)) stubBase + s"/penalties-stub/appeals/submit?enrolmentKey=$enrolmentKey&isLPP=$isLPP&penaltyNumber=$penaltyNumber"
+    if (!isEnabled(CallPEGA)) stubBase + s"/income-tax-penalties-stubs/appeals/submit?enrolmentKey=$enrolmentKey&isLPP=$isLPP&penaltyNumber=$penaltyNumber"
     else pegaBase + s"/penalty/first-stage-appeal/$penaltyNumber"
   }
 
